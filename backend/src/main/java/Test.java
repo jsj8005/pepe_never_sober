8b29968fbf243e75c4a9cd1f6eb8893e0f0fc69f
jsj <jsj8005@gmail.com>
2025-02-10 14:21:26 +0900

    first commit

---
+++ backend/src/main/java/Test.java
@@ -0,0 +1,8 @@
+import java.util.Optional;
+
+public class Test {
+    public static void main(String[] args) {
+        System.err.println("jsj");
+        String opt = Optional.of("asdf").orElseGet(() -> "1111");
+    }
+}
