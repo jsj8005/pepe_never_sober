e94e4fdf3f2eb2ce2c843436d6b7177213389dce
jsj <jsj8005@gmail.com>
2025-02-10 15:12:32 +0900

    수정2

--- backend/src/main/java/Test.java
+++ backend/src/main/java/Test.java
@@ -3,7 +3,6 @@ import java.util.Optional;
 public class Test {
     public static void main(String[] args) {
         System.err.println("jsj");
-        
         String opt = Optional.of("asdf").orElseGet(() -> "1111");
     }
 }
